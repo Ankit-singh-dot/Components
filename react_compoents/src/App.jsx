d2e605fb32ae6594e6174875b113b82523875d3c
Ankit <ankitrajdihara123@gmail.com>
2025-10-16 19:45:31 +0530

    Text_pop_up

---
+++ react_compoents/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TextLoop from "../component/TextLoop";
+
+function App() {
+  return (
+    <div>
+      <TextLoop />
+    </div>
+  );
+}
+
+export default App;
